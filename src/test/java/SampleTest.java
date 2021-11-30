2134c0928e037edad431a0ef32e16611dd514072
PaulBodin <pbodin@et.esiea.fr>
2021-11-30 14:12:11 +0100

    Mise à jour des tests

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,8 +1,11 @@
 package fr.lernejo;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class SampleTest {
+
+    private final Sample sample = new Sample();
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
@@ -18,5 +21,14 @@ class SampleTest {
 
     @org.junit.jupiter.api.Test
     void fact() {
+            Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> sample.fact(-1))
+                .withMessage("N should be positive"); // (3)
+        }
+
+    @Test
+    void fact_of_3_is_6() {
+        int result = sample.fact(3);
+        Assertions.assertThat(result).isEqualTo(6); // (4)
     }
 }
